9bc41382d2ec7a5255387a38803f6511dbbebf8b
Nolan Heimlich <54378167+Nolan01m@users.noreply.github.com>
2020-10-15 19:19:33 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+#aiexperts.com
+
+# Create a VPC
+resource "aws_vpc" "aiexperts" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "website" {
+  ami           = "ami-0a5ce0c6788a56cf4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Website"
+  }
+}
+
+# integrate lex
