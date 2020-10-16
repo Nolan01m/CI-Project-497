3bd6a475523a4e589e6fcd7d35d186150403953f
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-10-15 23:29:13 -0400

    .

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "r" {
   vpc_id = aws_vpc.aiexperts.id
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 }
