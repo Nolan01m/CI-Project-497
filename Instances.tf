20f476bb0aa8570671416f28e1d05a3d634ad4ab
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-10-17 11:22:07 -0400

    testing egress gateway

--- Instances.tf
+++ Instances.tf
@@ -1,56 +1,19 @@
-resource "aws_internet_gateway" "Gateway" {
-  vpc_id = aws_vpc.aiexperts.id
-}
-resource "aws_route_table" "Routing" {
-  vpc_id = aws_vpc.aiexperts.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.Gateway.id
-  }
-}
-//# Creating a Security Group
-resource "aws_security_group" "default" {
-  name        = "allow_http(s)"
-  description = "Allow http(s) inbound traffic"
-  vpc_id = aws_vpc.aiexperts.id
+//##Instances##\\
+//# Create an EC2 instance
+resource "aws_instance" "website" {
 
-  ingress {
-    protocol  = -1
-    from_port = 0
-    to_port   = 0
-    cidr_blocks = ["0.0.0.0/0"]
+  ami           = "ami-0a5ce0c6788a56cf4"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.Int.id
+    device_index         = 0
+    
   }
-  egress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-  tags = {
-    Name = "allow_http(s)"
-  }
-}
-resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.aiexperts.id
-  cidr_block        = "10.0.0.0/16"
-  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "aiexperts_subnet"
+    Name = "Website"
   }
 }
-resource "aws_network_interface" "Int" {
-  subnet_id   = aws_subnet.subnet.id
-  private_ips = ["10.0.100.0"]
-  security_groups = [aws_security_group.default.id]
 
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-resource "aws_eip" "External_IP" {
-  instance = aws_instance.website.id
-  vpc      = true
-  associate_with_private_ip = "10.0.100.0"
-  depends_on                = [aws_internet_gateway.Gateway]
-}
+//# Integrate lex 
--- Networking.tf
+++ Networking.tf
@@ -1,19 +1,63 @@
-//##Instances##\\
-//# Create an EC2 instance
-resource "aws_instance" "website" {
+resource "aws_internet_gateway" "Gateway" {
+  vpc_id = aws_vpc.aiexperts.id
+}
+resource "aws_egress_only_internet_gateway" "Egress" {
+  vpc_id = aws_vpc.example.id
+}
+resource "aws_route_table" "Routing" {
+  vpc_id = aws_vpc.aiexperts.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Gateway.id
+        }
+  route {
+    ipv6_cidr_block = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.Egress.id
+        }
+}
+//# Creating a Security Group
+resource "aws_security_group" "default" {
+  name        = "allow_http(s)"
+  description = "Allow http(s) inbound traffic"
+  vpc_id = aws_vpc.aiexperts.id
 
-  ami           = "ami-0a5ce0c6788a56cf4"
-  instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.Int.id
-    device_index         = 0
-    
+  ingress {
+    protocol  = -1
+    from_port = 0
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
   tags = {
-    Name = "Website"
+    Name = "allow_http(s)"
   }
 }
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.aiexperts.id
+  cidr_block        = "10.0.0.0/16"
+  availability_zone = "us-east-1a"
 
-//# Integrate lex 
+  tags = {
+    Name = "aiexperts_subnet"
+  }
+}
+resource "aws_network_interface" "Int" {
+  subnet_id   = aws_subnet.subnet.id
+  private_ips = ["10.0.100.0"]
+  security_groups = [aws_security_group.default.id]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+resource "aws_eip" "External_IP" {
+  instance = aws_instance.website.id
+  vpc      = true
+  associate_with_private_ip = "10.0.100.0"
+  depends_on                = [aws_internet_gateway.Gateway]
+}
