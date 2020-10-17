300a129b123caa0c87565d936a13e006dff87a29
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-10-17 19:07:34 -0400

    ..

--- Main.tf
+++ Main.tf
@@ -18,3 +18,22 @@ resource "aws_vpc" "aiexperts" {
     Name = "AIExperts"
   }
 }
+//##Instances##\\
+//# Create an EC2 instance
+resource "aws_instance" "website" {
+
+  ami           = "ami-0a5ce0c6788a56cf4"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.Int.id
+    device_index         = 0
+    
+  }
+
+  tags = {
+    Name = "Website"
+  }
+}
+
+//# Integrate lex 
